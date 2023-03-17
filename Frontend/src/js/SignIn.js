b171d69b7eb04b4f571c528a631fc11e05c41f41
Cameron-Whyte <2382684w@student.gla.ac.uk>
2023-03-17 16:49:56 +0000

    SignIn CSS updated

--- Frontend/src/js/SignIn.js
+++ Frontend/src/js/SignIn.js
@@ -101,19 +101,12 @@ return (
   <ThemeProvider theme={theme}>
     <Grid container component="main" sx={{ height: '100vh' }}>
       <CssBaseline />
-      <Grid item xs={12} sm={4} md={7} className="background-image"
-        style={{backgroundImage: `url(${publicPath+logoPath})`}}>
+      <Grid item xs={12} sm={4} md={7} 
+	className="background-image"
+        style={{backgroundImage: `url(${publicPath+logoPath})`,  position: 'relative'}}>
       </Grid>
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
+        <Box className="box">
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
@@ -167,5 +160,4 @@ return (
     </Grid>
   </ThemeProvider>
 );
-
 }
