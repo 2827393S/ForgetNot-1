4d0dd170e2ebd83076c429ac91ad5e79b32ccdba
Warrior-li <lishuxuan.0304@gmail.com>
2023-03-23 00:29:16 +0000

    HTTPS

--- Frontend/src/setupProxy.js
+++ Frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app){
     app.use(createProxyMiddleware('/api',{
-        target:"http://35.189.83.153:8000/",
+        target:"https://35.189.83.153:8000/",
         changeOrigin : true,
 
     }))
